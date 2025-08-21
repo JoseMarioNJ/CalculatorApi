21e7145a17ec8d4a068c09368b136f70c52863dd
Jose Mario Nuñez Justiniano <a2024111939@estudiantes.upsa.edu.bo>
2025-08-20 21:44:43 -0400

    .

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet("sum")]
+        public IActionResult GetSum([FromQuery] int a, [FromQuery] int b)
+
+        {
+            var result = a + b;
+            return Ok(new { a, b, result });
+
+        }
+
+        [HttpGet("rest")]
+        public IActionResult GetRest([FromQuery] int a, [FromQuery] int b)
+        {
+            var result = a - b;
+            return Ok(new { a, b, result });
+        }
+
+        [HttpGet("producto")]
+
+        public IActionResult GetProducto([FromQuery] int a, [FromQuery] int b)
+        {
+            var result = a * b;
+            return Ok(new { a, b, result });
+        }
+
+        [HttpGet("division")]
+        public IActionResult GetDivision([FromQuery] int a, [FromQuery] int b)
+        {
+            var result = a / b;
+            return Ok(new { a, b, result });
+        }
+    }
+}
